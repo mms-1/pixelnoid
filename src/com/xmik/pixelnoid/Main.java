1e48ee379a47d5914f275f493bb7f10673f3088b
mms <mms@mms-PC>
2016-11-06 03:51:23 +0300

    Change to pixelnoid

--- src/com/xmik/pixelnoid/Main.java
+++ src/com/xmik/pixelnoid/Main.java
@@ -1,10 +1,21 @@
 package com.xmik.pixelnoid;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Arkanoid!");
+		
+		/* TODO Somehow check if it does anything */
+		if (System.getProperty("org.lwjgl.librarypath") == null) {
+			Path path = Paths.get("native");
+			String librarypath = path.toAbsolutePath().toString();
+			System.setProperty("org.lwjgl.librarypath", librarypath);
+		}
+		
 		new Test().run();
 	}
 
